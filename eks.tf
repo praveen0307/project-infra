2419ae0ae1cd37305d8c364f6b3f383e0b314999
praveen.kesharwani <114658170+praveen0307@users.noreply.github.com>
2023-04-25 02:15:12 +0530

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_role.name
+}
+
+
+resource "aws_eks_cluster" "task_cluster" {
+  name     = "${var.cluster_name}-${var.region}"
+  role_arn = aws_iam_role.eks_role.arn
+  version = "1.20"
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
+}
